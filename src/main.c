6227be849e83ca93dfd0ab0f44a41a075a6aeb7b
Ricardo Augusto <ricardo.rals.dev@gmail.com>
2024-12-23 16:49:28 -0300

    refactor: Ajustando funcionalidade de conversão de potência para funcionar no arquivo main.c

--- src/main.c
+++ src/main.c
@@ -3,13 +3,15 @@
 
 void conversao_main();
 int dados_main();
+int potencia_main();
 
 int main() {
     int opcao;
 
     do {
         printf("Menu Principal:\n");
-        printf("3. Acessar o menu de conversão de unidades de volume\n");
+        printf("3. Acessar o menu de conversão de unidades de volume (litro, mililitro, metros cúbicos)\n");
+        printf("6. Acessar o menu de conversão Watts (W), quilowatts (kW), cavalos-vapor (cv ou hp)\n");
         printf("9. Acessar o menu de conversão Bits, bytes, kilobytes (KB), megabytes (MB), gigabytes (GB), terabytes (TB)\n");
         printf("0. Sair\n");
         printf("Digite a opção: ");
@@ -18,6 +20,9 @@ int main() {
         switch (opcao) {
             case 3:
                 conversao_main();  // Chama a função principal do arquivo de conversão
+                break;
+            case 6:
+                potencia_main();
                 break;
             case 9:
                 dados_main();
